e09bb1b33f2543ce489893d26b30e4c5e0d402c7
Plog <plog99@gmail.com>
2020-03-31 07:25:53 +0700

    styles

--- listall.php
+++ listall.php
@@ -5,7 +5,7 @@ usort( $fileList, function( $a, $b ) { return filemtime($b) - filemtime($a); } )
 foreach($fileList as $filename){
   $uname = preg_replace("/(.*)___(.*)___(.*)/", "$2", $filename);
   echo "<div class='images'>";
-  echo "<img src='$filename' width=110 height= alt='$uname' title='$uname'>"; 
+  echo "<img src='$filename' width=160 height= alt='$uname' title='$uname'>"; 
   echo "<label>$uname</label>";
   echo "</div>";
 }
